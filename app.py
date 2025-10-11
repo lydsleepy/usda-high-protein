ee213a772c45a918eb83b8374aa011edd5e8d3c3
lydsleepy <jiahua.mp3@gmail.com>
2025-10-11 16:36:57 -0500

    instructions

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ import pandas as pd
 import time
 import plotly.express as px
 
+'''
+to test run:
+streamlit run app.py
+
+Local URL: http://localhost:8501
+Network URL: http://10.148.82.87:8501
+'''
+
 st.title("Protein Analyzer")
 
 st.sidebar.header("Filters")
