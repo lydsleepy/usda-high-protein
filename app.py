663f1587635cb83d8fd1ea84777a86b817bf3c41
lydsleepy <jiahua.mp3@gmail.com>
2025-10-11 17:16:39 -0500

    commit

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ import requests
 import pandas as pd
 import time
 import plotly.express as px
-import 
+from usda_functions import fetch_category, extract_nutrients
 
-'''
-to test run:
-streamlit run app.py
+# to test run:
+# streamlit run app.py
 
-Local URL: http://localhost:8501
-Network URL: http://10.148.82.87:8501
-'''
+# Local URL: http://localhost:8501
+# Network URL: http://10.148.82.87:8501
 
+# do NOT use ''' comments it will show up on the website
+
+st.set_page_config(page_title="Protein Analyzer", page_icon="🐱", layout="wide")
 st.title("Protein Analyzer")
 
 st.sidebar.header("Filters")
+
